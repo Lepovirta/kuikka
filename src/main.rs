bc644291f69ab18cfdf062fbd3869c25db3215cd
Jaakko Pallari <jkpl@lepo.group>
2018-01-14 21:53:06 +0000

    Time and date

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 extern crate gtk;
 extern crate gdk;
+extern crate cairo;
+extern crate pango;
+extern crate chrono;
 
 use gtk::prelude::*;
 use gdk::prelude::*;
+use chrono::Local;
+
+const STYLE_TEMPLATE: &'static str = "
+.osmo-root {
+  margin: 20px;
+  color: white;
+}
+.osmo-time {
+  font-size: 80px;
+}
+.osmo-date {
+  font-size: 30px;
+}
+";
 
 fn main() {
     if gtk::init().is_err() {
@@ -10,27 +27,63 @@ fn main() {
         return;
     }
 
+    // Geometry
     let display = gdk::Display::get_default().unwrap();
     let monitor = display.get_primary_monitor().unwrap();
     let geometry = monitor.get_geometry();
-    let win_width = geometry.width / 4;
-    let win_height = geometry.height / 4;
-    let x_pos = geometry.width - win_width - win_width / 2;
-    let y_pos = win_height / 2;
+    let x_pos = geometry.width / 8;
+    let y_pos = geometry.height / 8;
 
-    let window = gtk::Window::new(gtk::WindowType::Popup);
+    // Style
+    let styles = gtk::CssProvider::new();
+    styles.load_from_data(STYLE_TEMPLATE.as_bytes()).unwrap();
+    let screen = gdk::Screen::get_default().unwrap();
+    gtk::StyleContext::add_provider_for_screen(
+        &screen,
+        &styles,
+        gtk::STYLE_PROVIDER_PRIORITY_FALLBACK
+    );
+
+    // Window
+    let window = gtk::Window::new(gtk::WindowType::Toplevel);
     window.set_title("osmo");
+    window.set_app_paintable(true);
+    window.set_type_hint(gdk::WindowTypeHint::Dock);
+    window.set_keep_above(true);
     window.move_(x_pos, y_pos);
-    window.set_default_size(win_width, win_height);
-    let label = gtk::Label::new("Hello world!");
 
-    window.add(&label);
-    window.show_all();
+    // Clock
+    let time_lbl = gtk::Label::new(None);
+    time_lbl.get_style_context().unwrap().add_class("osmo-time");
 
+    // Date
+    let date_lbl = gtk::Label::new(None);
+    date_lbl.get_style_context().unwrap().add_class("osmo-date");
+
+    // Container
+    let container = gtk::Box::new(gtk::Orientation::Vertical, 10);
+    container.get_style_context().unwrap().add_class("osmo-root");
+    container.add(&time_lbl);
+    container.add(&date_lbl);
+    window.add(&container);
+
+    // Ticker
+    let tick = move || {
+        let time = format!("{}", Local::now().format("%H:%M"));
+        let date = format!("{}", Local::now().format("%Y-%m-%d"));
+        time_lbl.set_text(&time);
+        date_lbl.set_text(&date);
+        gtk::Continue(true)
+    };
+    tick();
+    gtk::timeout_add_seconds(1, tick);
+
+    // Kickoff
     window.connect_delete_event(|_, _| {
         gtk::main_quit();
         Inhibit(false)
     });
+    window.show_all();
 
     gtk::main();
 }
